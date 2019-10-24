b68cf934d34657bb252cd1aa5c607421ffdde339
Victor Witkamp <victorwitkamp@gmail.com>
2019-10-24 08:47:58 +0200

    eventfix

--- src/Controllers/EventController.php
+++ src/Controllers/EventController.php
@@ -18,38 +18,50 @@ class EventController extends Controller
         parent::__construct();
 
         if (isset($_POST['addEvent'])) {
-            $title = (string) Request::post('title', true);
-            $start_event = (string) Request::post('start_event', true);
-            $end_event = (string) Request::post('end_event', true);
-            $description = (string) Request::post('description', true);
-            if (CalendarEventModel::create($title, $start_event, $end_event, $description)) {
-                Redirect::to('events/');
-            } else {
-                Redirect::error();
-            }
+            self::createHandler();
         }
 
         if (isset($_POST['updateEvent'])) {
-            $event_id = (int) Request::post('id', true);
-            $title = (string) Request::post('title', true);
-            $start_event = (string) Request::post('start_event', true);
-            $end_event = (string) Request::post('end_event', true);
-            $description = (string) Request::post('description', true);
-            $status = (int) Request::post('status', true);
-            if (CalendarEventModel::update($event_id, $title, $start_event, $end_event, $description, $status)) {
-                Redirect::to('events/');
-            } else {
-                Redirect::error();
-            }
+            self::updateHandler();
         }
 
         if (isset($_POST['deleteEvent'])) {
-            $id = (int) Request::post('id', true);
-            if (CalendarEventModel::delete($id)) {
-                Redirect::to('events/');
-            } else {
-                Redirect::error();
-            }
+            self::deleteHandler();
+        }
+    }
+
+    public static function deleteHandler() {
+        $id = (int) Request::post('id', true);
+        if (CalendarEventModel::delete($id)) {
+            Redirect::to('events/');
+        } else {
+            Redirect::error();
+        }
+    }
+
+    public static function updateHandler() {
+        $event_id = (int) Request::post('id', true);
+        $title = (string) Request::post('title', true);
+        $start_event = (string) Request::post('start_event', true);
+        $end_event = (string) Request::post('end_event', true);
+        $description = (string) Request::post('description', true);
+        $status = (int) Request::post('status', true);
+        if (CalendarEventModel::update($event_id, $title, $start_event, $end_event, $description, $status)) {
+            Redirect::to('events/');
+        } else {
+            Redirect::error();
+        }
+    }
+
+    public static function createHandler() {
+        $title = (string) Request::post('title', true);
+        $start_event = (string) Request::post('start_event', true);
+        $end_event = (string) Request::post('end_event', true);
+        $description = (string) Request::post('description', true);
+        if (CalendarEventModel::create($title, $start_event, $end_event, $description)) {
+            Redirect::to('events/');
+        } else {
+            Redirect::error();
         }
     }
 }
