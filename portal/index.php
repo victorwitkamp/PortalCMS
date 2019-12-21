90b68d1f453158b2fe3a6dc95ce2899daeac5f80
Victor Witkamp <victorwitkamp@gmail.com>
2019-12-21 17:10:27 +0100

    jaja

--- portal/index.php
+++ portal/index.php
@@ -5,5 +5,5 @@ if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/../src/Init.php')) {
     include $_SERVER['DOCUMENT_ROOT'] . '/../src/Init.php';
     new Application();
 } else {
-    echo 'Failed to initialize the application.';
+    echo 'Failed to initialize the application. Init.php does not exist.';
 }
