80caba68bdc45b8a1e1a83e4a0ae12405c922b27
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2020-01-31 14:52:33 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/View/Pages/Settings/SiteSettings.php
+++ src/View/Pages/Settings/SiteSettings.php
@@ -18,7 +18,7 @@ $pageName = Text::get('TITLE_SITE_SETTINGS'); ?>
                     </div>
                 </div>
                 <?php Alert::renderFeedbackMessages();
-                require __DIR__ .'inc/SiteSettingsGeneral.php'; ?>
+                require __DIR__ . 'inc/SiteSettingsGeneral.php'; ?>
             </form>
         </div>
 
