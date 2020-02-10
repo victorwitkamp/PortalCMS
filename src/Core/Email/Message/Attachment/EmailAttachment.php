8f7d7c64ca4c799a0f29cf643fb2a6fc435d587b
Victor Witkamp <victorwitkamp@gmail.com>
2020-02-10 19:06:05 +0100

    oops

--- src/Core/Email/Message/Attachment/EmailAttachment.php
+++ src/Core/Email/Message/Attachment/EmailAttachment.php
@@ -64,10 +64,9 @@ class EmailAttachment
         } elseif (!empty($mailId) && empty($templateId)) {
             // No implementation yet
             Session::add('feedback_negative', Text::get('FEEDBACK_MAIL_ATTACHMENT_UPLOAD_FAILED'));
-        } elseif (empty($mailId) && !empty($templateId)) && EmailAttachmentMapper::createForTemplate($templateId, $this)) {
-                Session::add('feedback_positive', Text::get('FEEDBACK_MAIL_ATTACHMENT_UPLOAD_SUCCESSFUL'));
-                return true;
-            }
+        } elseif (empty($mailId) && !empty($templateId) && EmailAttachmentMapper::createForTemplate($templateId, $this)) {
+            Session::add('feedback_positive', Text::get('FEEDBACK_MAIL_ATTACHMENT_UPLOAD_SUCCESSFUL'));
+            return true;
         }
         return false;
     }
