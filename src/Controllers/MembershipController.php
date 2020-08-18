86120c2bad555aff0f69ce246d1c1dacbc97f8a7
Victor Witkamp <victorwitkamp@gmail.com>
2020-08-18 07:01:16 +0200

    ohja

--- src/Controllers/MembershipController.php
+++ src/Controllers/MembershipController.php
@@ -91,17 +91,44 @@ class MembershipController extends Controller
 
     public static function saveMember() : void
     {
-        if (MemberModel::updateMember(new Member((int)Request::post('id', true), (int)Request::post('jaarlidmaatschap', true), Request::post('voorletters', true), Request::post('voornaam', true), Request::post('achternaam', true), Request::post('geboortedatum', true), new MemberAddress(Request::post('adres', true), Request::post('postcode', true), Request::post('huisnummer', true), Request::post('woonplaats', true)), new MemberContactDetails(Request::post('telefoon_vast', true), Request::post('telefoon_mobiel', true), Request::post('emailadres', true)), Request::post('ingangsdatum', true), Request::post('geslacht', true), new MemberPreferences((int)Request::post('nieuwsbrief', true), (int)Request::post('vrijwilliger', true), (int)Request::post('vrijwilligeroptie1', true), (int)Request::post('vrijwilligeroptie2', true), (int)Request::post('vrijwilligeroptie3', true), (int)Request::post('vrijwilligeroptie4', true), (int)Request::post('vrijwilligeroptie5', true)), new MemberPaymentDetails((string)Request::post('betalingswijze', true), (string)Request::post('iban', true), (string)Request::post('machtigingskenmerk', true), (int)Request::post('status', true))))) {
-            Session::add('feedback_positive', 'Lid opgeslagen.');
-        } else {
-            Session::add('feedback_negative', 'Lid opslaan mislukt.');
-        }
+        MemberModel::updateMember(
+            new Member(
+                (int) Request::post('id', true),
+                (int) Request::post('jaarlidmaatschap', true),
+                Request::post('voorletters', true),
+                Request::post('voornaam', true),
+                Request::post('achternaam', true),
+                Request::post('geboortedatum', true),
+                new MemberAddress(Request::post('adres', true), Request::post('postcode', true), Request::post('huisnummer', true), Request::post('woonplaats', true)),
+                new MemberContactDetails(Request::post('telefoon_vast', true), Request::post('telefoon_mobiel', true), Request::post('emailadres', true)),
+                Request::post('ingangsdatum', true),
+                Request::post('geslacht', true),
+                new MemberPreferences((int) Request::post('nieuwsbrief', true), (int) Request::post('vrijwilliger', true), (int) Request::post('vrijwilligeroptie1', true), (int) Request::post('vrijwilligeroptie2', true), (int) Request::post('vrijwilligeroptie3', true), (int) Request::post('vrijwilligeroptie4', true), (int) Request::post('vrijwilligeroptie5', true)),
+                new MemberPaymentDetails((string) Request::post('betalingswijze', true), (string) Request::post('iban', true), (string) Request::post('machtigingskenmerk', true), (int) Request::post('status', true))
+            )
+        );
         Redirect::to('membership/');
     }
 
     public static function saveNewMember() : void
     {
-        MemberModel::createMember(new Member(null, (int)Request::post('jaarlidmaatschap', true), Request::post('voorletters', true), Request::post('voornaam', true), Request::post('achternaam', true), Request::post('geboortedatum', true), new MemberAddress(Request::post('adres', true), Request::post('postcode', true), Request::post('huisnummer', true), Request::post('woonplaats', true)), new MemberContactDetails(Request::post('telefoon_vast', true), Request::post('telefoon_mobiel', true), Request::post('emailadres', true)), Request::post('ingangsdatum', true), Request::post('geslacht', true), new MemberPreferences((int)Request::post('nieuwsbrief', true), (int)Request::post('vrijwilliger', true), (int)Request::post('vrijwilligeroptie1', true), (int)Request::post('vrijwilligeroptie2', true), (int)Request::post('vrijwilligeroptie3', true), (int)Request::post('vrijwilligeroptie4', true), (int)Request::post('vrijwilligeroptie5', true)), new MemberPaymentDetails((string)Request::post('betalingswijze', true), (string)Request::post('iban', true), (string)Request::post('machtigingskenmerk', true), (int)Request::post('status', true))));
+        MemberModel::createMember(
+            new Member(
+                null,
+                (int) Request::post('jaarlidmaatschap', true),
+                Request::post('voorletters', true),
+                Request::post('voornaam', true),
+                Request::post('achternaam', true),
+                Request::post('geboortedatum', true),
+                new MemberAddress(Request::post('adres', true), Request::post('postcode', true), Request::post('huisnummer', true), Request::post('woonplaats', true)),
+                new MemberContactDetails(Request::post('telefoon_vast', true), Request::post('telefoon_mobiel', true), Request::post('emailadres', true)),
+                Request::post('ingangsdatum', true),
+                Request::post('geslacht', true),
+                new MemberPreferences((int) Request::post('nieuwsbrief', true), (int) Request::post('vrijwilliger', true), (int) Request::post('vrijwilligeroptie1', true), (int) Request::post('vrijwilligeroptie2', true), (int) Request::post('vrijwilligeroptie3', true), (int) Request::post('vrijwilligeroptie4', true), (int) Request::post('vrijwilligeroptie5', true)),
+                new MemberPaymentDetails((string) Request::post('betalingswijze', true), (string) Request::post('iban', true), (string) Request::post('machtigingskenmerk', true), (int) Request::post('status', true))
+            )
+        );
+        Redirect::to('membership/');
     }
 
     public static function deleteMembersById() : void
--- src/Modules/Members/MemberModel.php
+++ src/Modules/Members/MemberModel.php
@@ -46,7 +46,7 @@ class MemberModel
             return false;
         }
         $member = self::getMember($id);
-        if ($member->jaarlidmaatschap === $targetYear) {
+        if (($member === null) || ($member->jaarlidmaatschap === $targetYear)) {
             return false;
         }
         $member->id = null;
@@ -54,29 +54,23 @@ class MemberModel
         $member->paymentDetails->status = 0;
         $member->creationDate = null;
         $member->modificationDate = null;
-
         return (MemberMapper::new($member));
     }
 
-    /**
-     * @param int|null $id
-     */
     public static function getMember(int $id = null): ?Member
     {
-        if ($id === null) {
-            return null;
+        if ($id !== null) {
+            $membermap = MemberMapper::getMemberById($id);
+            if ($membermap !== null) {
+                return new Member($id, $membermap->jaarlidmaatschap, $membermap->voorletters, $membermap->voornaam, $membermap->achternaam, $membermap->geboortedatum, new MemberAddress($membermap->adres, $membermap->postcode, $membermap->huisnummer, $membermap->woonplaats), new MemberContactDetails($membermap->telefoon_vast, $membermap->telefoon_mobiel, $membermap->emailadres), $membermap->ingangsdatum, $membermap->geslacht, new MemberPreferences($membermap->nieuwsbrief, $membermap->vrijwilliger, $membermap->vrijwilligeroptie1, $membermap->vrijwilligeroptie2, $membermap->vrijwilligeroptie3, $membermap->vrijwilligeroptie4, $membermap->vrijwilligeroptie5), new MemberPaymentDetails($membermap->betalingswijze, $membermap->iban, $membermap->machtigingskenmerk, $membermap->status));
+            }
         }
-        $membermap = MemberMapper::getMemberById($id);
-        return new Member($id, $membermap->jaarlidmaatschap, $membermap->voorletters, $membermap->voornaam, $membermap->achternaam, $membermap->geboortedatum, new MemberAddress($membermap->adres, $membermap->postcode, $membermap->huisnummer, $membermap->woonplaats), new MemberContactDetails($membermap->telefoon_vast, $membermap->telefoon_mobiel, $membermap->emailadres), $membermap->ingangsdatum, $membermap->geslacht, new MemberPreferences($membermap->nieuwsbrief, $membermap->vrijwilliger, $membermap->vrijwilligeroptie1, $membermap->vrijwilligeroptie2, $membermap->vrijwilligeroptie3, $membermap->vrijwilligeroptie4, $membermap->vrijwilligeroptie5), new MemberPaymentDetails($membermap->betalingswijze, $membermap->iban, $membermap->machtigingskenmerk, $membermap->status));
+        return null;
     }
 
-    /**
-     * @param int $id
-     * @return bool
-     */
     public static function delete(int $id = null): bool
     {
-        if (MemberMapper::doesMemberIdExist($id)) {
+        if (($id !== null) && MemberMapper::doesMemberIdExist($id)) {
             if (MemberMapper::delete($id)) {
                 Session::add('feedback_positive', 'Lid verwijderd (ID: '.$id.').');
                 return true;
@@ -90,27 +84,24 @@ class MemberModel
 
     public static function updateMember(Member $member = null): bool
     {
-        if ($member === null) {
-            return false;
+        if (($member !== null) && MemberMapper::updateMember($member)) {
+            Session::add('feedback_positive', 'Lid opgeslagen.');
+            return true;
         }
-        return (MemberMapper::updateMember($member));
+        Session::add('feedback_positive', 'Lid niet opgeslagen.');
+        return false;
     }
 
-    /**
-     * @param Member|null $member
-     */
-    public static function createMember(Member $member = null)
+    public static function createMember(Member $member = null): bool
     {
-        if (MemberMapper::doesEmailforYearExist($member->jaarlidmaatschap, $member->contactDetails->emailadres)) {
-            Session::add('feedback_negative', 'Emailadres wordt dit jaar al gebruikt door een ander lid.');
-            Redirect::to('membership/');
-        } else {
-            if (MemberMapper::new($member)) {
-                Session::add('feedback_positive', 'Lid toegevoegd.');
-                Redirect::to('membership/');
-            }
+        if (($member === null)) {
             Session::add('feedback_negative', 'Lid toevoegen mislukt.');
-            Redirect::to('membership/');
+        } elseif (($member->jaarlidmaatschap !== null) && ($member->contactDetails->emailadres !== null) && MemberMapper::doesEmailforYearExist($member->jaarlidmaatschap, $member->contactDetails->emailadres)) {
+            Session::add('feedback_negative', 'Emailadres wordt dit jaar al gebruikt door een ander lid.');
+        } elseif (MemberMapper::new($member)) {
+            Session::add('feedback_positive', 'Lid toegevoegd.');
+            return true;
         }
+        return false;
     }
 }
